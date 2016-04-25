0cd5979fcb5684a7064392c12d0eda37b375af28
Jean-François Coulon <Jean-Francois.Coulon@imag.fr>
2016-04-25 17:58:46 +0200

    [ADD][MISSING]: Platform.hpp for proper build with previous commit

---
+++ Lib/public_include/Platform.hpp
@@ -0,0 +1,37 @@
+//===========================================================================
+//                           Platform.hpp
+//===========================================================================
+// Copyright (C) 2016 G-INP/Tima. All rights reserved.
+//
+// Project : Mast
+//
+//! @file Platform.hpp
+//!
+//! Defines platform dependant stuffs
+//===========================================================================
+
+
+#ifndef PLATFORM_H__31EE6B85_BB3D_4B14_E288_56C12101BFE0__INCLUDED_
+  #define PLATFORM_H__31EE6B85_BB3D_4B14_E288_56C12101BFE0__INCLUDED_
+
+  #define WINDOWS
+  #if defined(WINDOWS)
+    #include <windows.h>
+    #ifdef BUILD_DLL
+        #define DLL_EXPORT __declspec(dllexport)
+    #else
+        #define DLL_EXPORT __declspec(dllimport)
+    #endif
+  #else
+      #define DLL_EXPORT
+  #endif
+
+
+
+#endif  // not defined PLATFORM_H__31EE6B85_BB3D_4B14_E288_56C12101BFE0__INCLUDED_
+//===========================================================================
+// End of Platform.hpp
+//===========================================================================
+
+
+

c788cfd6af0d94d8b3e73ff0ad2acfa5b6eca2c3
Jean-François Coulon <Jean-Francois.Coulon@imag.fr>
2016-04-26 09:51:02 +0200

    [BUILD][ADD]: Makefile generated using cbp2make

--- MakeMakefiles.bat
+++ MakeMakefiles.bat
@@ -2,8 +2,15 @@
 
 @set tool_path="..\cbp2make\bin\Release\cbp2make.exe"
 
-@REM %tool_path% -in NewMast.workspace -out ws_makefile.mak      -unix -windows
-@REM %tool_path% -in Lib/Lib.cbp       -out Lib/lib.mak -unix -windows
-%tool_path% -in Lib/Lib.cbp       -out Lib/lib_Windows.mak -windows
-%tool_path% -in Lib/Lib.cbp       -out Lib/lib_Linux.mak -unix
+:Workspace
+%tool_path% --verbose -in NewMast.workspace -out workspace.mak      -windows -unix
+@REM %tool_path% --verbose -in NewMast.workspace -out ws_Windows.mak      -windows
+@REM %tool_path% --verbose -in NewMast.workspace -out ws_Linux.mak        -unix
 
+:Lib
+@REM %tool_path% --verbose -in Lib/Lib.cbp       -out Lib/lib_Windows.mak -windows
+@REM %tool_path% --verbose -in Lib/Lib.cbp       -out Lib/lib_Linux.mak   -unix
+
+:Lib_UT
+@REM %tool_path% --verbose -in Lib_UT/Lib_UT.cbp       -out Lib_UT/lib_UT_Windows.mak -windows
+@REM %tool_path% --verbose -in Lib_UT/Lib_UT.cbp       -out Lib_UT/lib_UT_Linux.mak   -unix

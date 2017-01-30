f22ca3dc54724470713720a6afa107c63403d7d9
Jean-François Coulon <Jean-Francois.Coulon@imag.fr>
2017-01-30 12:34:47 +0100

    [SIT_READER][FIX]: Catch all exceptions while parsing SIT. Especially catch exceptions due to syntax errors.

--- SIT_reader/SIT_reader.cpp
+++ SIT_reader/SIT_reader.cpp
@@ -3,6 +3,8 @@
 #include <cassert>
 #include <memory>
 
+#include "g3log/g3log.hpp"
+#include "Utility.hpp"
 #include "SystemModelBuilder.hpp"
 #include "SIT_reader.hpp"
 #include "SIT_parser.tab.hh"
@@ -16,22 +18,28 @@ using namespace mast;
 
 SIT::SIT_Reader::SIT_Reader( std::shared_ptr<mast::SystemModel> sm)
 {
- main_sm = sm;
+ main_sm    = sm;
  parsed_sut = nullptr;
- builder = make_shared<mast::SystemModelBuilder>(*main_sm);
+ builder    = make_shared<mast::SystemModelBuilder>(*main_sm);
 }
 
 bool
-SIT::SIT_Reader::parse( string_view filename )
+SIT::SIT_Reader::parse(string_view filename)
 {
-   assert( filename.data() != nullptr );
+  if (filename.empty())
+  {
+    LOG(ERROR_LVL) << "Must specify a valid file path";
+    return false;
+  }
 
-   std::ifstream in_file( filename.data() );
-   if( ! in_file.good() )
-   {
-      return false;
-   }
-   return parse_helper( in_file );
+  std::ifstream sitFile(filename.data());
+  if (!sitFile.good())
+  {
+    LOG(WARNING) << "Cannot open file:" << filename;
+    return false;
+  }
+
+  return parse_helper(sitFile);
 }
 
 bool
@@ -41,7 +49,7 @@ SIT::SIT_Reader::parse( std::istream &stream )
    {
        return false;
    }
-   //else
+
    return parse_helper( stream );
 }
 
@@ -49,35 +57,48 @@ SIT::SIT_Reader::parse( std::istream &stream )
 bool
 SIT::SIT_Reader::parse_helper( std::istream &stream )
 {
-   try
-   {
-      scanner = make_shared<SIT_Scanner>(&stream);
-   }
-   catch( std::bad_alloc &ba )
-   {
-      std::cerr << "Failed to allocate scanner: (" <<
-         ba.what() << "), exiting!!\n";
-      return false;
-   }
+  try
+  {
+    try
+    {
+       scanner = make_shared<SIT_Scanner>(&stream);
+    }
+    catch( std::bad_alloc &ba )
+    {
+       std::cerr << "Failed to allocate scanner: ("
+                 << ba.what() << "), exiting!!\n";
+       return false;
+    }
 
-   try
-   {
-      parser = make_shared<SIT_Parser>(*scanner /* scanner */, *this /* driver */);
-   }
-   catch( std::bad_alloc &ba )
-   {
-      std::cerr << "Failed to allocate parser: (" <<
-         ba.what() << "), exiting!!\n";
-      return false;
-   }
+    try
+    {
+       parser = make_shared<SIT_Parser>(*scanner /* scanner */, *this /* driver */);
+    }
+    catch( std::bad_alloc &ba )
+    {
+       std::cerr << "Failed to allocate parser: ("
+                 << ba.what() << "), exiting!!\n";
+       return false;
+    }
 
-   constexpr int accept = 0;
-   if( parser->parse() != accept )
-   {
-      std::cerr << "Parse failed!!\n";
-      return false;
-   }
-   return true;
+    constexpr int accept = 0;
+    if (parser->parse() == accept)
+    {
+      return true;
+    }
+  }
+  catch(std::exception& exc)  // Catch C++ standard exceptions (do nothing because it is the normal way error are reported)
+  {
+//+    std::cerr << "Got exception while parsing: " << exc.what();
+//+    LOG(ERROR_LVL) << "Got exception while parsing: " << exc.what();
+  }
+  catch (...)
+  {
+    LOG(ERROR_LVL) << "Got unknown type of exception";
+  }
+
+  std::cerr << "Parse failed!!\n";
+  return false;
 }
 
 void

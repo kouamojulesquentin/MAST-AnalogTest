e8de5e3268a18c5f629731cc79b963ca8d6feaa1
Jean-François Coulon <Jean-Francois.Coulon@univ-grenoble-alpes.fr>
2017-02-24 18:41:44 +0100

    [PROTOCOL][REMOTE][HOUPS]: Missing from previous commit

---
+++ Optional_Libs/MastXmlRpc/XmlRpc_Protocol_Common.hpp
@@ -0,0 +1,29 @@
+//===========================================================================
+//                           XmlRpc_Protocol_Common.hpp
+//===========================================================================
+// Copyright (C) 2017 G-INP/Tima. All rights reserved.
+//
+// Project : Mast
+//
+//! @file XmlRpc_Protocol_Common.hpp
+//!
+//! Declares commonalities between client and server classes
+//!
+//===========================================================================
+
+
+#ifndef XMLRPC_PROTOCOL_COMMON_H__772E6CE1_403F_44B6_63AB_26D8E15D7EF6__INCLUDED_
+  #define XMLRPC_PROTOCOL_COMMON_H__772E6CE1_403F_44B6_63AB_26D8E15D7EF6__INCLUDED_
+
+
+#define XML_RPC_COMMAND_DO_RESET         "XmlRpc_Protocol_Server.DoReset"
+#define XML_RPC_COMMAND_SEND_SCAN_VECTOR "XmlRpc_Protocol_Server.SendScanVector"
+
+
+#endif  // not defined XMLRPC_PROTOCOL_COMMON_H__772E6CE1_403F_44B6_63AB_26D8E15D7EF6__INCLUDED_
+//===========================================================================
+// End of XmlRpc_Protocol_Common.hpp
+//===========================================================================
+
+
+

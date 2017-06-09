42d0eee691510bc9807d75c9ee77799a1232a0be
Jean-François Coulon <Jean-Francois.Coulon@univ-grenoble-alpes.fr>
2017-06-09 18:26:12 +0200

    [MAST_APP]: Introduce Mast application (based upon MastEnvironment)

---
+++ Mast_App/Mast_App.cpp
@@ -0,0 +1,47 @@
+//===========================================================================
+//                           Mast_App.cpp
+//===========================================================================
+// Copyright (C) 2017 G-INP/Tima. All rights reserved.
+//
+// Project : Mast
+//
+//! @file Mast_App.cpp
+//!
+//! Implements Mast main function
+//!
+//===========================================================================
+
+#include "MastEnvironment.hpp"
+#include <stdexcept>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+using namespace mast;
+
+//! Runs Mast according to command line arguments
+//!
+int main (int argc, const char* argv[])
+{
+  int retCode = 0;
+  try
+  {
+    MastEnvironment mast;
+
+    mast.ParseOptions(argc, argv);
+    mast.LoadPlugins();
+  }
+  catch(std::exception& exc) { retCode = -1; cout << "Got: " << exc.what() << endl; }
+  catch(...)                 { retCode = -1; cout << "Got non std::exception" << endl; }
+
+  return retCode;
+}
+//
+//  End of: main
+//---------------------------------------------------------------------------
+
+
+//===========================================================================
+// End of Mast_App.cpp
+//===========================================================================

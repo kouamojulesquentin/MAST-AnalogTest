5f3bf8bade81a4c224606ee880e82acbe8c62997
Michele Portolan <michele.portolan@grenoble-inp.fr>
2017-11-07 15:59:41 +0100

    [ARM] Set of compilation and export flow for ARM target. Validated on Zynq

---
+++ Export_MAST_for_arm.cmake
@@ -0,0 +1,69 @@
+# This files describes what should be, minimally exported to execute MAST on an ARM
+# running on RHEL5
+
+set(ARM_Build_EXPORT ${EXPORT_DIR}/ARM_Build)
+
+message("")
+message("================================================================================")
+message(STATUS "Export_ARM: ARM_Build_EXPORT:          ${ARM_Build_EXPORT}")
+
+install(DIRECTORY   ${CMAKE_BINARY_DIR}/Lib
+        COMPONENT   ARM_Build
+        DESTINATION ${ARM_Build_EXPORT})
+
+#install(DIRECTORY   ${CMAKE_SOURCE_DIR}/External_Libs/xmlrpc-c/build/lib/
+#        COMPONENT   ARM_Build
+#        DESTINATION ${ARM_Build_EXPORT}/External_Libs/xmlrpc-c/)
+
+install(DIRECTORY   ${CMAKE_BINARY_DIR}/Bin/
+        COMPONENT   ARM_Build
+        DESTINATION ${ARM_Build_EXPORT}/Bin
+	PATTERN     "*"
+	PERMISSIONS OWNER_WRITE OWNER_READ  OWNER_EXECUTE
+	            GROUP_READ GROUP_EXECUTE
+		    WORLD_READ WORLD_EXECUTE
+	)
+
+install(FILES   ${CMAKE_SOURCE_DIR}/Run.sh
+        COMPONENT   ARM_Build
+	PERMISSIONS OWNER_WRITE OWNER_READ  OWNER_EXECUTE
+	            GROUP_READ GROUP_EXECUTE
+		    WORLD_READ WORLD_EXECUTE
+        DESTINATION ${ARM_Build_EXPORT})
+
+install(DIRECTORY   ${CMAKE_SOURCE_DIR}/MastExample_CPP/SIT/
+        COMPONENT   ARM_Build
+        DESTINATION ${ARM_Build_EXPORT}/SIT/)
+ 
+
+get_filename_component(Gxx_PATH ${CMAKE_CXX_COMPILER} DIRECTORY)
+
+SET(CMAKE_FIND_ROOT_PATH /opt/Xilinx/petalinux/tools/linux-i386/gcc-arm-linux-gnueabi/bin)
+
+ set (STD_LIBS_PATH "${CMAKE_FIND_ROOT_PATH}/../arm-linux-gnueabihf/libc/lib/")
+
+message(STATUS "Export_ARM: Setting local STD_LIBS_PATH")
+
+install(FILES   ${STD_LIBS_PATH}libgcc_s.so.1 ${STD_LIBS_PATH}libstdc++.so.6.0.22 
+        COMPONENT   ARM_Build
+        DESTINATION ${ARM_Build_EXPORT}/Std_libs)
+
+#INSTALL(CODE "execute_process( \
+#    COMMAND ${CMAKE_COMMAND} -E create_symlink -r\
+#    ${CMAKE_INSTALL_PREFIX}/${ARM_Build_EXPORT}/Std_libs/libstdc++.so.6.0.22 \
+#    ${CMAKE_INSTALL_PREFIX}/${ARM_Build_EXPORT}/Std_libs/libstdc++.so.6   \
+#    )"
+#)
+
+#Manually create a simlink to be used in dynamic library linking (i.e. with LD_LIBRARY_PATH)
+execute_process(COMMAND ln -rs ${CMAKE_INSTALL_PREFIX}/${ARM_Build_EXPORT}/Std_libs/libstdc++.so.6.0.22  ${CMAKE_INSTALL_PREFIX}/${ARM_Build_EXPORT}/Std_libs/libstdc++.so.6)
+
+install(FILES   ${CMAKE_INSTALL_PREFIX}/${ARM_Build_EXPORT}/Std_libs/libstdc++.so.6
+        COMPONENT   ARM_Build
+        DESTINATION ${ARM_Build_EXPORT}/Std_libs)
+
+#message(STATUS "Export_ARM: STD_LIBS_PATH:          ${STD_LIBS_PATH}")
+message("================================================================================")
+message("")
+
+

56b0370ca4b7f162bed7231f581801679b53006d
Jean-François Coulon <Jean-Francois.Coulon@univ-grenoble-alpes.fr>
2017-12-15 16:18:50 +0100

    [MAST_APP][WORKAROUND]: Avoid failures when running MAST with ICL files. Failure seems to be due to corruption due to probably DLL difference regarding ICL_Reader. This is just a workaround, since I HAVE NOT FOUND THE ROOT CAUSE of the issue.                                  =============================== The effective workaround is at line 30 of Mast_App.cpp

--- Mast_App/Mast_App.cpp
+++ Mast_App/Mast_App.cpp
@@ -20,6 +20,18 @@ using std::endl;
 
 using namespace mast;
 
+
+//+ (begin JFC December/15/2017): Workaround ==> It is useful to avoid corrupted istream while ICL_Lexer operate (right at the start of parsing
+//+                               I've not found the real cause of the problem, so there is a high risk that it will blew up again
+//+                               I've only seen the problem for Linux but I'm not using the same gcc version (4.9.3 on Windows, 5.4 on Linux)
+#include "ICL_Reader.hpp"
+#include <memory>
+std::shared_ptr<mast::SystemModel> sm_not_used;
+ICL::ICL_Reader                    workaround(sm_not_used);
+//+ (end   JFC December/15/2017):
+
+
+
 //! Runs Mast according to command line arguments
 //!
 int main (int argc, const char* argv[])

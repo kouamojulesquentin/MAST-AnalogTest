830cb5c37f5f92b024d5773afb0908c154b8f18f
Jean-François Coulon <Jean-Francois.Coulon@univ-grenoble-alpes.fr>
2018-02-20 11:27:32 +0100

    [TUTORIALS][ADD]: For ICL_Tutorial_2, move back aliases into Instrument.icl. They were moved to WrappedInstr.icl while this case was not yet supported.

--- Tutorials/ICL_Tutorial_2/ICL_Tutorial_2.cpp
+++ Tutorials/ICL_Tutorial_2/ICL_Tutorial_2.cpp
@@ -34,25 +34,23 @@ void DoTest_Modes (string_view registerPath)
   auto red           = "4'b0011"sv;
   auto green         = "4'b0100"sv;
   auto blue          = "4'b1000"sv;
-  auto previousColor = "4'b0000"sv;
 
   for (const auto& color : {red, green, blue})
   {
     iWrite(registerPath, color);
 
+    LOG(INFO) << "PDL algorithm Wrote color: " << color       << " to   " << registerPath;
+    iApply();
+
     auto curValue    = iGet(registerPath, StringType::Binary);
     auto curValueBV  = BinaryVector::CreateFromString(curValue);
     auto curValueIcl = curValueBV.DataAsICLMixString();
-
-    LOG(INFO) << "PDL algorithm Wrote color: " << color       << " to   " << registerPath;
     LOG(INFO) << "PDL algorithm Read:        " << curValueIcl << " from " << registerPath;
-    iApply();
 
-    if (curValueIcl != previousColor)
+    if (curValueIcl != color)
     {
-      LOG(ERROR_LVL) << "Failed to read back previous color " << previousColor << " from " << registerPath;
+      LOG(ERROR_LVL) << "Failed to read back color " << color << " from " << registerPath << ", got " << curValueIcl;
     }
-    previousColor = color;
   }
 }
 //
@@ -63,20 +61,29 @@ void DoTest_Modes (string_view registerPath)
 //! Sets data register chunk with different values
 //!
 //! @note Data is taken from same register than another test for modes !
-void DoTest_Data (string_view registerPath)
+void DoTest_Data (string_view instancePath)
 {
+  // Lamba: Gets current register value as ICL formatted string
+  auto iGetAsIcl = [instancePath](auto registerPath)
+  {
+    auto current    = iGet(registerPath, StringType::Binary);
+    auto currentBV  = BinaryVector::CreateFromString(current);
+    auto currentIcl = currentBV.DataAsICLMixString();
+    LOG(INFO) << "PDL algorithm Read:        " << currentIcl << " from " << instancePath << "." << registerPath;
+    return currentIcl;
+  };
+
+  iPrefix(instancePath);
+  auto registerPath = "data"sv;
   auto previousData = "3'b001"sv; // LSB is one because register is reset with value 1 that is also LSB of data !
 
   for (const auto& data : {"3'b000"sv, "3'b001"sv, "3'b010"sv, "3'b110"sv})
   {
     iWrite(registerPath, data);
+    LOG(INFO) << "PDL algorithm Wrote data:  " << data        << " to   " << instancePath << "." << registerPath;
 
-    auto curValue    = iGet(registerPath, StringType::Binary);
-    auto curValueBV  = BinaryVector::CreateFromString(curValue);
-    auto curValueIcl = curValueBV.DataAsICLMixString();
+    auto curValueIcl = iGetAsIcl(registerPath);
 
-    LOG(INFO) << "PDL algorithm Wrote data:  " << data        << " to   " << registerPath;
-    LOG(INFO) << "PDL algorithm Read:        " << curValueIcl << " from " << registerPath;
     iApply();
 
     if (curValueIcl != previousData)
@@ -84,6 +91,20 @@ void DoTest_Data (string_view registerPath)
       LOG(ERROR_LVL) << "Failed to read back previous data " << previousData << " from " << registerPath;
     }
     previousData = data;
+  }
+
+  // ---------------- Check effect of last write
+  //
+  auto curOkayIcl = iGetAsIcl("okay");  // okay is on bit 0
+  auto curDoneIcl = iGetAsIcl("done");  // done is on bit 1
+  if (curOkayIcl != "1'b0")
+  {
+    LOG(ERROR_LVL) << "Failed to read back proper value for okay, got \"" << curOkayIcl << "\" instead of \"1'b0\"";
+  }
+
+  if (curDoneIcl != "1'b1")
+  {
+    LOG(ERROR_LVL) << "Failed to read back proper value for okay, got \"" << curDoneIcl << "\" instead of \"1'b1\"";
   }
 }
 //
@@ -107,7 +128,7 @@ bool RegisterAlgorithms ()
   //
   repo.RegisterAlgorithm("Test_Modes_1", []() { DoTest_Modes("Daisy.WI1.mode"sv); });
   repo.RegisterAlgorithm("Test_Modes_2", []() { DoTest_Modes("Daisy.WI2.mode"sv); });
-  repo.RegisterAlgorithm("Test_Data",    []() { DoTest_Data ("Daisy.WI2.data"sv); });
+  repo.RegisterAlgorithm("Test_Data",    []() { DoTest_Data ("Daisy.WI2"sv); });
 
   return true;
 }
--- Tutorials/ICL_Tutorial_2/SReg.icl
+++ Tutorials/ICL_Tutorial_2/SReg.icl
@@ -1,8 +1,19 @@
 Module SReg
 {
-  Parameter Size = 8;
-  ScanInPort    SI;
-  ScanOutPort   SO          { Source  reg[0];}
+  Parameter   Size = 8;
+  ScanInPort  SI;
+  ScanOutPort SO   { Source reg[0];}
+
+  DataInPort  DI[$Size - 1:0];
+  DataOutPort DO[$Size - 1:0] { Source reg; }
+
   ScanInterface scan_client { Port SI; Port SO; }
-  ScanRegister reg[$Size - 1:0]    { ScanInSource SI; ResetValue 'b1; }
+
+  ScanRegister reg[$Size - 1:0]
+  {
+    ScanInSource  SI;
+    CaptureSource DI;
+    ResetValue    'b1;
+  }
+
 }

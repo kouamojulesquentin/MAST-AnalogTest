85ea2920197856dd68c11f617da3a237472a10b0
Michele Portolan <michele.portolan@grenoble-inp.fr>
2020-03-26 11:41:59 +0100

    [P1687.1] added the Empty_TranslatorProtocol as an example to write custom Translators           with their own factory

---
+++ Optional_Libs/Empty_TranslatorProtocol/Empty_ProtocolFactory.cpp
@@ -0,0 +1,55 @@
+//===========================================================================
+//                           Trivum_ProtocolFactory.cpp
+//===========================================================================
+// Copyright (C) 2020 G-INP/Tima. All rights reserved.
+//
+// Project : Mast
+//
+//! @file Empty_ProtocolFactory.cpp
+//!
+//! Implements class Empty_EmulationProtocol
+//!
+//===========================================================================
+
+#include "Empty_TranslatorProtocol.hpp"
+#include "AccessInterfaceTranslatorProtocolFactory.hpp" 
+#include "g3log/g3log.hpp"
+
+#include "Utility.hpp"
+#include <thread>
+#include <chrono>
+#include <memory>
+ 
+using std::experimental::string_view;
+using std::string;
+using std::ofstream;
+using std::ifstream;
+using namespace mast;
+using namespace std::string_literals;
+
+namespace
+{
+  //! Registers the AccessInterface protocol
+  //!
+  //! @note Names used from registration must be the same as found in SIT file
+  bool AccessInterfaceTranslatorProtocolFactory ()
+  {
+    auto& repo     = AccessInterfaceTranslatorProtocolFactory::Instance();
+    auto  factory  = [](const string& /* parameters */)  { return std::make_unique<Empty_TranslatorProtocol>(); };
+
+
+   repo.RegisterCreator("Empty", factory);
+
+    return true;
+  }
+
+  //! Make the AccessInterface protocol effectivelly registered
+  //!
+  //! @note As a "static" variable, it is initialized once when the corresponding DLL is loaded
+  //!
+  static bool registered = AccessInterfaceTranslatorProtocolFactory();
+} // End of unnamed namespace
+
+//===========================================================================
+// End of EmptyProtocolFactory.cpp
+//===========================================================================
---
+++ Optional_Libs/Empty_TranslatorProtocol/Empty_TranslatorProtocol.cpp
@@ -0,0 +1,86 @@
+//===========================================================================
+//                           Emulation_TranslationProtocol.cpp
+//===========================================================================
+// Copyright (C) 2020 G-INP/Tima. All rights reserved.
+//
+// Project : Mast
+//
+//! @file Empty_TranslatorProtocol.cpp
+//!
+//! Implements class Empty_TranslatorProtocol, which can be used as a model
+//! for real translators with its own Factory registration. 
+//! As it is, it only transmits requests and results with no modification and is therefore functionally transparent
+//!
+//===========================================================================
+
+#include "Empty_TranslatorProtocol.hpp"
+#include "Utility.hpp"
+#include "CallbackRequest.hpp"
+
+#include <experimental/string_view>
+#include <sstream>
+
+#include "g3log/g3log.hpp"
+
+using namespace mast;
+using std::string;
+using std::experimental::string_view;
+using std::ostringstream;
+
+using std::vector;
+using std::initializer_list;
+
+using namespace std::string_literals;
+using namespace std::experimental::literals::string_view_literals;
+
+//! Initializeation with no parameters
+//!
+Empty_TranslatorProtocol::Empty_TranslatorProtocol ()
+{
+}
+//
+//  End of: Empty_TranslatorProtocol::Empty_TranslatorProtocol
+//---------------------------------------------------------------------------
+
+
+BinaryVector Empty_TranslatorProtocol::TransformationCallback(CallbackRequest current_request) 
+{
+  // This callback is called each time a CallbackRequest arrived
+  
+  BinaryVector higher_level_result;
+  
+   //Syncrhonisation
+   if (current_request.CallbackId()==NO_MORE_PENDING)
+   {
+    //Finished, release parent Translator
+    CallbackRequest request(NO_MORE_PENDING);
+    PushRequest(request);
+    return higher_level_result;
+   }
+
+   //Process "current_request"
+  // Prepare a request to the higher-level interface: 
+ 
+  //Just pass request without modifying it
+  CallbackRequest higher_level_request=current_request;
+  
+  //Push request to higher level
+  PushRequest(higher_level_request);
+  
+//wait for Result from higher level; it is a BLOCKING call
+   higher_level_result = PopfromSut(); 
+  
+  //Process "result"
+  auto lower_level_result=higher_level_result;
+  
+  //Return Callback result to lower level
+  return lower_level_result;
+}
+//
+//  End of: Empty_TranslatorProtocol::TransformationCallback
+//---------------------------------------------------------------------------
+
+
+//===========================================================================
+// End of Empty_TranslatorProtocol.cpp
+//===========================================================================
